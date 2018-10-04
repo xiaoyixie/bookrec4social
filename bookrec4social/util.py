490e3c20c24bbee13973f36da9ca0bbb3eafa46b
Xiaoyi Xie <xxy1900@yahoo.com>
2018-10-04 10:16:14 -0400

    update

---
+++ bookrec4social/util.py
@@ -0,0 +1,328 @@
+import requests
+from xml.etree import ElementTree
+import os
+import sys
+import numpy as np
+import pandas as pd
+import scipy
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
+from sklearn.decomposition import TruncatedSVD
+from collections import defaultdict
+from numpy import linalg as LA
+
+# Custom libraries
+# import secret  # need to make this and add goodreads_api key
+
+not_found_error_message = "That username doesn't seem to exist on Goodreads, I'm sorry"
+private_error_message = "This user account is private, I'm sorry"
+no_ratings_error_message = "You don't have any ratings on the books we have access to, I'm sorry"
+
+
+
+def get_id_from_username(username, api_key):
+    response = requests.get('https://www.goodreads.com/user/show/?key=' + api_key + '&username=' + username + '&format=xml')
+    tree = ElementTree.fromstring(response.content)
+    try:
+        user_id = tree.find('user').find('id').text
+        return user_id
+    except:
+        # raise ValueError('Invalid Goodreads username, not id returned')
+        return None
+
+
+def get_user_vector(user_input, mapper):
+    """ Gets the user ratings vector of a user
+
+    Args:
+        user_input::str
+            username of the user
+        mapper::dict
+            maps the goodreads book id to our ids
+
+    Returns:
+        user_vector::np.array
+            an array of 10000 ratings for the given user
+        error_message::str
+            an error message string, if there is an error
+    """
+    try:
+        sparse_q = scipy.sparse.load_npz('static/data/cached_users/user_' + user_input + '.npz')
+        q = sparse_q.toarray()
+        q = np.array(q[0].tolist())
+        print('found user_vector...')
+        return q, None
+    except:
+        q = np.zeros((10000), dtype = np.float)
+        # api_key = secret.API_KEY
+        api_key = '4nbvcBRfYZ1MSL06ARuw'
+        if not user_input.isdigit():
+            user_id = get_id_from_username(user_input, api_key)
+        else:
+            user_id = user_input
+
+        if user_id is None:
+            return None, not_found_error_message
+
+        page = 1
+        total_valid_reviews = 0
+        while True:
+            response = requests.get('https://www.goodreads.com/review/list/?v=2&id=' + user_id + '&shelf=read&format=xml&key=' + api_key + '&per_page=200&page=' + str(page))
+            tree = ElementTree.fromstring(response.content)
+            reviews = tree.find('reviews')
+            if reviews is None:
+                return None, private_error_message
+            for review in reviews:
+                goodreads_book_id = str(review.find('book').find('id').text)
+                if goodreads_book_id in mapper:
+                    book_id = int(mapper[goodreads_book_id])
+                    rating = int(review.find('rating').text)
+                    q[book_id - 1] = float(rating)
+                    total_valid_reviews += 1
+            page += 1
+
+            print(len(reviews))
+            if len(reviews) < 1:
+                break
+
+        print("total valid reviews: %s" % (total_valid_reviews))
+        if total_valid_reviews < 1:
+            return None, no_ratings_error_message
+
+        q = feature_scaling(q)
+
+        # Disable this until we find a 'smart' caching solution
+        # print('saving user_vector...')
+        # scipy.sparse.save_npz('static/data/cached_users/user_'+user_input+'.npz', scipy.sparse.csr_matrix(q))
+
+        return q, None
+
+
+def get_friends_information(user_input, q, mapper):
+
+
+    """ Gets the user ratings vector of a user
+
+    Args:
+        user_input::str
+            username of the user
+        mapper::dict
+            maps the goodreads book id to our ids
+
+    Returns:
+        user_vector::np.array
+            an array of 10000 ratings for the given user
+        error_message::str
+            an error message string, if there is an error
+    """
+    from lxml import html
+    password = '320501xxy'
+    session_requests = requests.session()
+
+    login_url = 'https://www.goodreads.com/'
+    result  = session_requests.get(login_url)
+    tree = html.fromstring(result.text)
+    authenticity_token = list(set(tree.xpath("//input[@name='authenticity_token']/@value")))[0]
+    print(authenticity_token)
+    payload = {
+        "user[email]": "xyx1811@gmail.com",
+        "user[password]": password,
+        "authenticity_token" : authenticity_token
+        }        
+    result = session_requests.post(
+        login_url,
+        data = payload,
+        headers = dict(referer=login_url)
+        )
+    
+    users = []
+    # url="https://www.goodreads.com/user/show/14215152-denisa"
+    # url="https://www.goodreads.com/user/show/16737045-jeanny"
+
+    url="https://www.goodreads.com/user/show/18903098-daniel-penev"
+    result = session_requests.get(url, headers = dict(referer = url))
+    tree = html.fromstring(result.content)
+    friend_names=tree.xpath("//div[@class='friendName']/a/text()")
+    friend_ids=tree.xpath("//div[@class='friendName']/a/@href")
+    friend_images=tree.xpath("/div.bigBoxBody/img/@src")
+    friend_ids=[ ''.join(list(filter(str.isdigit, item))) for item in friend_ids]
+    print(friend_names)
+    print(friend_ids)
+    friend_class = {}
+    
+    friend_vec = np.zeros((10000), dtype = np.float)
+    ncount = 0
+    if len(friend_ids) == len(friend_names):
+        for ix in range(len(friend_ids)):
+            vec, err = get_user_vector(friend_ids[ix], mapper)
+            # print(err)
+            if err!=200 and err!=None:
+                # print(err)
+                continue
+            
+            ncount = ncount + 1
+            score = np.dot(vec, q)
+            # print(LA.norm(vec))
+            # score = score/(LA.norm(vec)*LA.norm(q))
+            print("score: %.2e"%(score))
+            print("#books: %d"%(len(np.nonzero(vec)[0])))
+            # print(score)
+            if score > 0:
+                friend_vec = np.add(vec, friend_vec)
+            friend_class[friend_ids[ix]] = {'ID':friend_ids[ix], 'Name':friend_names[ix], 'Score':score, 'Books':len(np.nonzero(vec)[0])}
+    print(friend_class)
+    
+    
+    #     for user in theseusers:
+    #         print(user)
+    #         if user not in users :
+    #             users.append(user)
+    
+    # try:
+    #     # sparse_q = scipy.sparse.load_npz('static/data/cached_users/user_' + user_input + '.npz')
+    #     # q = sparse_q.toarray()
+    #     # q = np.array(q[0].tolist())
+    #     # print('found user_vector...')
+    #     # return q, None
+    #     api_key = '4nbvcBRfYZ1MSL06ARuw'
+    #     if not user_input.isdigit():
+    #         user_id = get_id_from_username(user_input, api_key)
+    #     else:
+    #         user_id = user_input
+
+    #     if user_id is None:
+    #         return None, not_found_error_message        
+    # except:
+
+        
+    #     page = 1
+    #     total_valid_reviews = 0
+    #     while True:
+    #         response = requests.get('https://www.goodreads.com/review/list/?v=2&id=' + user_id + '&shelf=read&format=xml&key=' + api_key + '&per_page=200&page=' + str(page))
+    #         tree = ElementTree.fromstring(response.content)
+    #         reviews = tree.find('reviews')
+    #         if reviews is None:
+    #             return None, private_error_message
+    #         for review in reviews:
+    #             goodreads_book_id = str(review.find('book').find('id').text)
+    #             if goodreads_book_id in mapper:
+    #                 book_id = int(mapper[goodreads_book_id])
+    #                 rating = int(review.find('rating').text)
+    #                 q[book_id - 1] = float(rating)
+    #                 total_valid_reviews += 1
+    #         page += 1
+
+    #         print(len(reviews))
+    #         if len(reviews) < 1:
+    #             break
+
+    #     print("total valid reviews: %s" % (total_valid_reviews))
+    #     if total_valid_reviews < 1:
+    #         return None, no_ratings_error_message
+
+    #     q = feature_scaling(q)
+
+    #     # Disable this until we find a 'smart' caching solution
+    #     # print('saving user_vector...')
+    #     # scipy.sparse.save_npz('static/data/cached_users/user_'+user_input+'.npz', scipy.sparse.csr_matrix(q))
+    # session_requests.config['keep_alive'] = False
+    return friend_names, friend_class, friend_vec, ncount
+
+
+
+def feature_scaling(q):
+    """ Scales the user features using the mean and the
+    standard deviation.
+    """
+    if q.dtype != np.float:
+        q = q.astype(np.float)
+    nonzero = np.nonzero(q)
+    nonzero_ratings = q[nonzero]
+    mean = np.mean(nonzero_ratings)
+    std = np.std(nonzero_ratings)
+    print('Mean: %s' % (mean))
+    print('S.D: %s' % (std))
+    if std == 0:
+        q[nonzero] = (1.0 + q[nonzero] - mean)
+    else:
+        q[nonzero] = (1.0 + q[nonzero] - mean) / std
+    return q
+
+
+'''
+Recommender functions
+'''
+
+
+def chunker(top_books):
+    # chunk into groups of 3 to display better in web app
+    chunks = []
+    current_chunk = []
+    for i in range(len(top_books)):
+        if len(current_chunk) < 3:
+            current_chunk.append(top_books[i])
+        else:
+            chunks.append(current_chunk)
+            current_chunk = [top_books[i]]
+
+    chunks.append(current_chunk)
+    return chunks
+
+
+def get_books_from_indices(top_book_indices, books):
+    top_books = []
+    for i in range(len(top_book_indices)):
+        book_id = top_book_indices[i]
+        book = books.iloc[book_id - 1]  # index is book_id - 1
+        book['rank'] = i + 1
+
+        # for some reason, some of the text fields have newlines appended to them
+        book['title'] = book['title'].strip()
+        book['author'] = book['author'].strip()
+        top_books.append(book)
+    return top_books
+
+
+def get_top_n_recs(result, books, n, q):
+    recs = []
+    for i in range(len(result)):
+        if q[i] == 0:  # book user hasn't already rated
+            recs.append((i, result[i]))
+        else:
+            recs.append((i, float('-inf')))
+    recs = sorted(recs, key=lambda tup: tup[1], reverse=True)
+
+    top_books = []
+    for i in range(n):
+        book_id = recs[i][0]
+        book = books.iloc[book_id]
+        book['rank'] = i + 1
+
+        # for some reason, some of the text fields have newlines appended to them
+        book['title'] = book['title'].strip()
+        book['author'] = book['author'].strip()
+        top_books.append(book)
+
+    return top_books
+
+
+def most_popular(books, n):
+    top_books = []
+    for i in range(n):
+        book = books.iloc[i]
+        book['rank'] = i + 1
+
+        # for some reason, some of the text fields have newlines appended to them
+        book['title'] = book['title'].strip()
+        book['author'] = book['author'].strip()
+        top_books.append(book)
+
+    return top_books
+
+
+def map_user(q, V):
+    # map new user to concept space by q*V
+    user_to_concept = np.matmul(q, V)
+    # map user back to itme space with user_to_concept * VT
+    result = np.matmul(user_to_concept, V.T)
+    return result
---
+++ bookrec4social/views.py
@@ -0,0 +1,257 @@
+from flask import Flask, request, render_template, redirect, url_for
+import numpy as np
+import pandas as pd
+import logging
+import pickle
+import csv
+import os
+from bookrec4social import app
+
+# Custom libraries
+
+from bookrec4social.util import get_user_vector, chunker, get_top_n_recs, map_user, most_popular, get_books_from_indices, not_found_error_message, get_friends_information
+
+
+''' GLOBALS
+'''
+bookid_to_title = None
+title_to_bookid = None
+mapper_id = None
+item_matrix = None
+qi = None  # item to concept matrix from SVD
+top_recs_each_book_item_matrix = None
+top_recs_each_book_feature_matrix = None
+books = None
+titles = None
+currentpath = str(os.path.dirname(os.path.abspath(__file__)))
+
+''' DATA LOADING FUNCTIONS
+'''
+
+
+def load_books():
+    """ Loads in the books and titles from the pickled dataframe """
+    """['author', 'description', 'image_url', 'title', 'url']"""
+    global books, titles
+    if books is None or titles is None:
+        titles = []
+        books = pd.read_pickle(currentpath+'/static/data/books_dataframe')
+        for index, row in books.iterrows():
+            titles.append(row['title'])
+        titles.sort()
+        print('books loaded')
+
+
+def load_title_mappers():
+    """ Loads in the title mappers using books.csv """
+    global bookid_to_title, title_to_bookid
+    if bookid_to_title is None or title_to_bookid is None:
+        bookid_to_title = {}
+        title_to_bookid = {}
+        filename = currentpath+'/static/data/books.csv'
+        with open(filename, "r", encoding='utf8') as f:
+            reader = csv.reader(f, delimiter=",")
+            for i, line in enumerate(reader):
+                bookid_to_title[line[0]] = line[10]
+                title_to_bookid[line[10]] = line[0]
+        print('books mapper loaded')
+
+
+def load_id_mapper():
+    """ Loads in the id mapper using books.csv.
+
+    This maps goodreads book ids to our ids.
+    """
+    global mapper_id
+    if mapper_id is None:
+        mapper_id = {}
+        filename = currentpath+'/static/data/books.csv'
+        with open(filename, "r", encoding='utf8') as f:
+            reader = csv.reader(f, delimiter=",")
+            for i, line in enumerate(reader):
+                mapper_id[line[1]] = line[0]
+        print('mapper_id loaded')
+
+
+def load_item_matrix():
+    """ Loads in the item to concept matrix """
+    global item_matrix
+    if item_matrix is None:
+        item_matrix = np.load(currentpath+'/static/data/item_matrix.npy')
+        print('item matrix loaded')
+
+
+def load_top_recs_each_book():
+    global top_recs_each_book_item_matrix, top_recs_each_book_feature_matrix
+    if top_recs_each_book_feature_matrix is None or top_recs_each_book_item_matrix is None:
+        f = open(currentpath+'/static/data/top_recs_each_book_item_matrix.pkl', "rb")
+        top_recs_each_book_item_matrix = pickle.load(f)
+        f.close()
+
+        f = open(currentpath+'/static/data/top_recs_each_book_feature_matrix.pkl', "rb")
+        top_recs_each_book_feature_matrix = pickle.load(f)
+        f.close()
+        print('top recs for each book loaded')
+
+
+def load_svd_matrix():
+    global qi
+    if qi is None:
+        qi = np.load(currentpath+'/static/data/svd.npy')
+        print('svd matrix loaded')
+
+
+def load_data():
+    global titles
+    load_title_mappers()
+    load_id_mapper()
+    # load_svd_matrix()
+    load_books()
+    load_item_matrix()
+    # load_top_recs_each_book()
+    return render_template('book_list.html', titles=titles)
+
+
+''' HOME PAGE
+'''
+
+
+# @app.route('/')
+# def home():
+#     return render_template('start.html')
+
+
+# @app.route('/', methods=['POST'])
+# def home_post():
+#     if 'load' in request.form:
+#         return redirect(url_for('recommender'))
+
+
+''' SETUP PAGE
+'''
+
+
+# @app.route('/setup')
+# def setup():
+#     return render_template('setup.html')
+
+
+# @app.route('/setup', methods=['POST'])
+# def setup_post():
+#     if 'load' in request.form:
+#         return redirect(url_for('recommender'))
+
+
+''' RECOMMENDER PAGE
+'''
+
+
+@app.route('/')
+def home():
+    return load_data()
+
+
+@app.route('/', methods=['POST'])
+def home_post():
+    global item_matrix, books, title_to_bookid, cosine_sim_item_matrix, cosine_sim_feature_matrix
+    # if 'book_recs' in request.form:
+    #     text = request.form['books']
+
+    #     if text not in title_to_bookid:
+    #         return render_template('book_list.html',
+    #                                response=not_found_error_message,
+    #                                titles=titles)
+
+    #     book_id = int(title_to_bookid[text])
+    #     top_book_indices = top_recs_each_book_item_matrix[book_id]
+    #     top_books = get_books_from_indices(top_book_indices, books)
+    #     chunks = chunker(top_books)
+    #     return render_template('book_list.html',
+    #                            toPass=chunks,
+    #                            titles=titles,
+    #                            response='Showing Recommendations for: ' + text)
+
+    # if 'book_similar' in request.form:
+    #     text = request.form['books']
+
+    #     if text not in title_to_bookid:
+    #         return render_template('book_list.html',
+    #                                response=not_found_error_message,
+    #                                titles=titles)
+
+    #     book_id = int(title_to_bookid[text])
+    #     top_book_indices = top_recs_each_book_feature_matrix[book_id]
+    #     top_books = get_books_from_indices(top_book_indices, books)
+    #     chunks = chunker(top_books)
+    #     return render_template('book_list.html',
+    #                            toPass=chunks,
+    #                            titles=titles,
+    #                            response='Showing Similar Books to: ' + text)
+
+    if 'user_recs' in request.form:
+        text = request.form['text']
+
+        q, error_message = get_user_vector(text, mapper_id)
+        if error_message:
+            return render_template('book_list.html',
+                                   response=error_message,
+                                   titles=titles)
+
+        # Get recs using item_matrix
+        top_books = get_top_n_recs(map_user(q, item_matrix), books, 18, q)
+        chunks = chunker(top_books)
+
+        # Get recs using svd
+        # top_books = get_top_n_recs(map_user(q, qi), books, 99, q)
+
+        return render_template('book_list.html',
+                               toPass=chunks,
+                               titles=titles,
+                               # response='Showing Recommendations for: ' + text)
+                               response='')
+
+    if 'user_friends' in request.form:
+        text = request.form['text']
+        q, error_message = get_user_vector(text, mapper_id)
+        
+        if error_message:
+            return render_template('book_list.html',
+                                   response=error_message,
+                                   titles=titles)
+
+        # Get recs using item_matrix
+        friend_name, friend_class, friend_vec, ncount = get_friends_information(text, q, mapper_id)
+        print(ncount)
+        # print(np.sum(friend_vec))
+        # print(np.sum(q))
+        
+        if ncount == 0:
+            q_new = np.add(q, 0)
+            return render_template('book_list.html',
+                               toPass=chunks,
+                               titles=titles,
+                               # response='Showing Recommendations for: ' + text)
+                                response='Not enough friend data collected',
+                                friends=friend_name )
+            
+        else:
+            q_new = np.add(q, friend_vec)
+            top_books = get_top_n_recs(map_user(q_new, item_matrix), books, 18, q_new)
+            chunks = chunker(top_books)
+            return render_template('book_list.html',
+                                       toPass=chunks,
+                                       titles=titles,
+                                       response='Showing Recommendations for: ' + text + ' & friends')
+        
+
+    # if 'most_popular' in request.form:
+    #     top_books = most_popular(books, 99)
+    #     chunks = chunker(top_books)
+    #     return render_template('book_list.html',
+    #                            toPass=chunks,
+    #                            titles=titles,
+    #                            response='99 Most Popular Books')
+    else:
+        return 'ERROR'
+
+
